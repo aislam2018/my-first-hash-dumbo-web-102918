50c2343ced11f4f1a24a4a20bbc6f12dfe5fb2d6
anik1606 <anik1606@gmail.com>
2018-10-15 19:43:42 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -9,9 +9,9 @@ end
 
 def shipping_manifest
   the_manifest = {}
-  the_manifest["whale_bone_corsets"] = 5
-  the_manifest["porcelain_vases"] = 2 
-  the_manifest["oil_paintings"] = 3
+  the_manifest["whale bone corsets"] = 5
+  the_manifest["porcelain vases"] = 2 
+  the_manifest["oil paintings"] = 3
   return the_manifest
 end
 
